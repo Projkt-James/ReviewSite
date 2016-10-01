49e35e4c743f5ba775af3516e047e6871f708278
James <James@projkt.io>
2016-10-01 17:45:30 +1000

    Cleared dir before Laravel Install

--- pages/logout.php
+++
@@ -1,8 +0,0 @@
-<?php 
-session_start(); //start a session 
-//include "../includes/activity.php"; ACTIVITY TODO
-//clearActivity();
-
-session_destroy(); //destroy all sessions 
-header('location:index.php'); 
-?> 
